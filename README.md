1695d4b522c829597febc90037f8dc312dddd458
Carsten Jacobsen <car.jacobsen@gmail.com>
2021-06-15 14:07:59 +0200

    feat(initial): initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
+
+# @kvass/scroll-anchor
+
+## Docs
+
+No docs yet
---
+++ src/Component.vue
@@ -0,0 +1,60 @@
+<template>
+  <span
+    class="scroll-anchor"
+    :class="['scroll-anchor--type-' + type, 'scroll-anchor--value-' + value]"
+    :style="{ top: offset }"
+  ></span>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+    },
+    type: {
+      type: String,
+      enum: ['anchor', 'path'],
+      default: 'anchor',
+    },
+    offset: String,
+    delay: {
+      type: Number,
+      default: 0,
+    },
+    scrollOptions: {
+      type: Object,
+      default: () => ({ behavior: 'smooth' }),
+    },
+  },
+  watch: {
+    value: 'check',
+    type: 'check',
+  },
+  methods: {
+    getValue() {
+      switch (this.type) {
+        case 'anchor':
+          return window.location.hash.substring(1)
+        case 'path':
+          return window.location.pathname
+      }
+    },
+    check() {
+      if (this.getValue() !== this.value) return
+      setTimeout(() => this.$el.scrollIntoView(this.scrollOptions), this.delay)
+    },
+  },
+  mounted() {
+    this.check()
+  },
+}
+</script>
+
+<style lang="scss">
+.scroll-anchor {
+  display: inline-block;
+  position: relative;
+  pointer-events: none;
+}
+</style>
